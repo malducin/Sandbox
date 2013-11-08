a861c1eebbcd6cc9a50ebe263f6c1a778220a22b
malducin <malducin@gmail.com>
2013-11-08 09:10:37 -0800

    Add Department and Customer classes

--- CSharpGenerics/CSharpGenerics/Program.cs
+++ CSharpGenerics/CSharpGenerics/Program.cs
@@ -10,7 +10,17 @@ namespace CSharpGenerics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hallo Git from Visual Studion");
+            var employeesByName = new SortedList<string, List<Employee>>();
+
+            employeesByName.Add("Sales", new List<Employee> { new Employee(), new Employee(), new Employee() });
+            employeesByName.Add("Engineering", new List<Employee> { new Employee(), new Employee() });
+
+            foreach (var item in employeesByName)
+            {
+                Console.WriteLine("The count of employees for {0} is {1}",
+                            item.Key, item.Value.Count
+                        );
+            }
         }
     }
 }
