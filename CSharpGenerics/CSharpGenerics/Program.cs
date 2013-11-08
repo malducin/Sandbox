f088bf3e272fdb730e2646bb82e3dca9de4e9fda
malducin <malducin@gmail.com>
2013-11-07 23:46:52 -0800

    Initial Version - Do Commit

---
+++ CSharpGenerics/CSharpGenerics/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Git from Visual Studion")
+        }
+    }
+}
